ca51598aede8eb66cb314cde1017a4500fb12ce0
sapiangkasa <andika.nma@gmail.com>
2014-12-13 07:05:40 +0800

    mothersheep movement

---
+++ Assets/AngleChange.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var _transform : Transform;
+var moveSpeed : float = 0.01;
+
+function Start () {
+	_transform = transform;
+}
+
+function Update () {
+	if(Input.GetKey("a"))
+		_transform.rotation.z += moveSpeed*Time.deltaTime;
+	else if(Input.GetKey("d"))
+		_transform.rotation.z -= moveSpeed*Time.deltaTime;
+}
