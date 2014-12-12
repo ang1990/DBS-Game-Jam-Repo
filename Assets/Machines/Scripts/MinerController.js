0d62d771ccf482c1deba71f807a0be1eba93d68e
ang1990 <ang1990@hotmail.com>
2014-12-13 01:47:53 +0800

    Basic Machine Controller Classes
    Not sure how to animate the explosion.

---
+++ Assets/Machines/Scripts/MinerController.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+class MinerController extends MachineController {
+
+var moneyAmount : float = 20;
+
+var profitTime : float = 2;
+private var timeSinceLastMoney : float;
+
+
+function operateMachine() {
+	if(Time.timeSinceLevelLoad > timeSinceLastMoney + profitTime) {
+		addMoney(moneyAmount);
+		timeSinceLastMoney = Time.timeSinceLevelLoad;
+	}	
+}
+
+function addMoney(amount : float) {
+
+}
+
+}
---
+++ Assets/Machines/Scripts/TempMachineController.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+class TempMachineController extends MachineController {
+
+enum TempMode {Raise, Lower};
+
+var tempStrength : float = 0.5;
+var optimalTemp : float = 50;
+var currentTemp : float;
+
+private var mode : TempMode;
+
+function setMachineVars() {
+	mode = TempMode.Raise;
+	currentTemp = 100;
+}
+
+function operateMachine() {
+	mode = currentTemp > optimalTemp ? TempMode.Lower : TempMode.Raise;
+	switch(mode) {
+		case TempMode.Raise :
+			raiseTemp(tempStrength * updateTime);
+			break;
+		case TempMode.Lower :
+			lowerTemp(tempStrength * updateTime);
+			break;
+		default:
+			break;
+	}
+	
+}
+
+function raiseTemp(amount : float) {
+}
+
+function lowerTemp(amount : float) {
+}
+
+
+}
