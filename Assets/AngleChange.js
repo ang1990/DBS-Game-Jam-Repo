aff88f483ac5ba87b9e4cfd897877c1d8e0878bb
sapiangkasa <andika.nma@gmail.com>
2014-12-13 08:22:26 +0800

    anchor adjustment
    anchor adjust
    mothersheep change direction

--- Assets/AngleChange.js
+++ Assets/AngleChange.js
@@ -2,14 +2,19 @@
 
 var _transform : Transform;
 var moveSpeed : float;
+var mothersheep : Transform;
 
 function Start () {
 	_transform = transform;
 }
 
 function Update () {
-	if(Input.GetKey("a"))
+	if(Input.GetKey("a")) {
 		_transform.Rotate(0,0,moveSpeed*Time.deltaTime);
-	else if(Input.GetKey("d"))
+		mothersheep.localScale.x = -1;
+	}
+	else if(Input.GetKey("d")) {
 		_transform.Rotate(0,0,-moveSpeed*Time.deltaTime);
+		mothersheep.localScale.x = 1;
+	}
 }
