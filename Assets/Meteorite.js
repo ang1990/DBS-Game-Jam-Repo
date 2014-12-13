587dd8f9a81c8bb1b28476859531fde46acc1897
ang1990 <ang1990@hotmail.com>
2014-12-13 20:15:18 +0800

    Timer and Meteor done.

--- Assets/Meteorite.js
+++ Assets/Meteorite.js
@@ -1,21 +1,29 @@
 ﻿#pragma strict
 
 var XDistTravel : float;
+var initialX : float;
 
 var timeBetweenUpdates : float = 0.05;
 var timeSinceLastUpdate : float;
 
 var maxTime : float;
+var timeLeft : float;
 
 var gameController : GameController;
 
 function Start () {
-	XDistTravel = GameObject.Find("Red Timer").renderer.bounds.size.x;
+	XDistTravel = (GameObject.Find("Red Timer").renderer.bounds.size.x) / 2;
+	initialX = transform.position.x - (0.5 * renderer.bounds.size.x);
 	gameController = GameObject.Find("Environment").GetComponent(GameController);
+	maxTime = 10;// gameController.getTimeLeft();
+	timeLeft = maxTime;
 }
 
 function Update () {
 	if(Time.timeSinceLevelLoad > timeSinceLastUpdate + timeBetweenUpdates) {
-		maxTime = 0;
+			timeLeft -= (Time.timeSinceLevelLoad - timeSinceLastUpdate);
+			var progress : float = 1 - timeLeft/maxTime;
+			transform.position.x =  initialX + (progress * XDistTravel);
+			timeSinceLastUpdate = Time.timeSinceLevelLoad;
 		}
 }	
