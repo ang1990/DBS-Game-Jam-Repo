5d193fecd37ed047c75ef2b884db21b07e032338
sapiangkasa <andika.nma@gmail.com>
2014-12-13 19:20:26 +0800

    Mothersheep rocket

--- Assets/AngleChange.js
+++ Assets/AngleChange.js
@@ -6,25 +6,30 @@ var acceleration : float = 120;
 var currentSpeed : float = 0;
 var mothersheep : Transform;
 var deceleration : float = 120;
+var _animator : Animator;
 
 function Start () {
 	_transform = transform;
+	_animator = GetComponentInChildren(Animator);
 }
 
 function Update () {
 	if(Input.GetKey("a")) {
+		_animator.SetBool("Flight",true);
 		currentSpeed = Mathf.Min(currentSpeed + (acceleration * Time.deltaTime), maxSpeed);
 		if(currentSpeed < 0)
 			currentSpeed = Mathf.Min(currentSpeed + (deceleration * Time.deltaTime), maxSpeed);
 		mothersheep.localScale.x = -1;
 	}
 	else if(Input.GetKey("d")) {
+		_animator.SetBool("Flight",true);
 		currentSpeed = Mathf.Max(currentSpeed - (acceleration * Time.deltaTime), -maxSpeed);
 		if(currentSpeed > 0)
 			currentSpeed = Mathf.Max(currentSpeed - (deceleration * Time.deltaTime), -maxSpeed);
 		mothersheep.localScale.x = 1;
 	}
 	else {
+		_animator.SetBool("Flight",false);
 		if(currentSpeed > 0) {
 			currentSpeed = Mathf.Max(0, currentSpeed - (deceleration * Time.deltaTime));
 		}
