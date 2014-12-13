fe177eed2a205d6267dbc99f000ead85dce38809
ang1990 <ang1990@hotmail.com>
2014-12-13 13:27:55 +0800

    MachineController WIP

--- Assets/Player/Scripts/Deployment.js
+++ Assets/Player/Scripts/Deployment.js
@@ -10,37 +10,44 @@ var coldMachineCost : int;
 var hotMachineCost : int;
 var ecoMachineCost : int;
 
+var anchorTransform : Transform;
+
 var playerResources : PlayerResources;
 
 function Start() {
-	playerResources = GameObject.Find("mothersheep").GetComponent(PlayerResources);
+	playerResources = GameObject.Find("Mothersheep").GetComponent(PlayerResources);
+	anchorTransform = transform.parent;
+}
+
+function Update() {
+	if(Input.GetKeyDown("v")) {
+		deployMiner();
+	}
 }
 
 function deployMiner() {
 	if(playerResources.getMoney() >= minerCost) {
 		playerResources.reduceMoney(minerCost);
-		Instantiate(miner);
+		Instantiate(miner,Vector3.zero,Quaternion.Euler(0,0,anchorTransform.eulerAngles.z));
 	}
 }
 
 function deployColdMachine() {
 	if(playerResources.getMoney() >= coldMachineCost) {
 		playerResources.reduceMoney(coldMachineCost);
-		Instantiate(coldMachine);
+		Instantiate(coldMachine,Vector3.zero,Quaternion.Euler(0,0,anchorTransform.eulerAngles.z));
 	}
 }
 
 function deployHotMachine() {
 	if(playerResources.getMoney() >= hotMachineCost) {
 		playerResources.reduceMoney(hotMachineCost);
-		Instantiate(hotMachine);
+		Instantiate(hotMachine,Vector3.zero,Quaternion.Euler(0,0,anchorTransform.eulerAngles.z));
 	}
 }
 function deployEcoMachine() {
 	if(playerResources.getMoney() >= ecoMachineCost) {
 		playerResources.reduceMoney(ecoMachineCost);
-		Instantiate(ecoMachine);
+		Instantiate(ecoMachine,Vector3.zero,Quaternion.Euler(0,0,anchorTransform.eulerAngles.z));
 	}
 }
-
->>>>>>> origin/master
