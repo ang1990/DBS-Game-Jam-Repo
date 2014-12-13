eb5367f7b4e47449c98d61eeb7dacc95aa04f12d
sapiangkasa <andika.nma@gmail.com>
2014-12-13 12:59:58 +0800

    deploy miner

---
+++ Assets/MinerBehaviour.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+private var _transform : Transform;
+private var _animator : Animator;
+private var state : int;
+
+function Start () {
+	state = 1;
+	_transform = transform;
+	_animator = gameObject.GetComponent(Animator);
+	_animator.SetBool("mine", false);
+	
+}
+
+function Update () {
+	if (state == 1) {
+		if (_transform.localPosition.y < 51) {
+			state = 2;
+		}
+		_transform.localPosition.y -= 1;
+	}
+	
+	if (state == 2) {
+		if (!_animator.GetBool("mine"))
+			_animator.SetBool("mine",true);
+	}
+}
--- Assets/Player/Scripts/Deployment.js
+++ Assets/Player/Scripts/Deployment.js
@@ -1,9 +1,23 @@
 ﻿#pragma strict
 
-function Start () {
+var MinerPrefab : GameObject;
+var EcoPrefab : GameObject;
+var HeatPrefab : GameObject;
+var ColdPrefab : GameObject;
 
+var _anchor : Transform;
+
+function Start () {
+	_anchor = transform.parent;
 }
 
 function Update () {
+	
+	if(Input.GetKeyDown("v")) {
+		Deployment (MinerPrefab);
+	}
+}
 
+function Deployment  (prefab : GameObject) {
+	Instantiate (prefab, Vector3(0,0,0),Quaternion.Euler(0,0,_anchor.eulerAngles.z));
 }
