fe665d04de5a6c38e8166365d9a9c4dd63aaaa7d
Sense <gryphsoft@live.com>
2014-12-13 17:48:31 +0800

    Number Display works. Finally.

---
+++ Assets/moneyDisplay.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var numArray : goldDigit[];
+var a : int;
+var playerResources : PlayerResources;
+
+function Start () {
+	playerResources = GameObject.Find("Mothersheep").GetComponent(PlayerResources);
+}
+
+function Update() {
+	setNumber(playerResources.getMoney());
+}
+
+function setNumber(no : int) {
+	var dispNumber = no;
+	for (var i : int = 0; i < numArray.Length; i++) {
+		numArray[i].setNumber(dispNumber%10);
+		dispNumber /= 10;
+	}
+}
