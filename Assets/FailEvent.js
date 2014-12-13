f0ab1b8a8af448f2a5b55bd3cff16502c2d6268f
sapiangkasa <andika.nma@gmail.com>
2014-12-13 20:49:51 +0800

    racommit

--- Assets/FailEvent.js
+++ Assets/FailEvent.js
@@ -1,7 +1,11 @@
 ﻿#pragma strict
+var imageRatio : float;
+var image : GUITexture;
 
 function Start () {
-
+	imageRatio = 25/15;
+	image.pixelInset.height = 0.9*Screen.height;
+	image.pixelInset.width = imageRatio * image.pixelInset.height;
 }
 
 function Update () {
