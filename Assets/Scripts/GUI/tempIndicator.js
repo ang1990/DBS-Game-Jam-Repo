9d3afb6bdb6445cc743dbb2b1893a1f8e7eacdee
Sense <gryphsoft@live.com>
2014-12-14 00:11:09 +0800

    Arrow for temperature

--- Assets/Scripts/GUI/tempIndicator.js
+++ Assets/Scripts/GUI/tempIndicator.js
@@ -1,28 +1,30 @@
 ﻿#pragma strict
 
-private var tempBar : GameObject;
+private var tempIndicator : GameObject;
 private var vertices : Vector3[];
 private var uv : Vector2[];
 private var Cam : GameObject;
-private var MAX_LEFT : float;
-private var MAX_RIGHT : float;
 private var MAX_TOP : float;
 private var MAX_BTM : float;
 private var height : float;
    
+private var posY : float;
 private var temp : Temperature;
 
+function SetHeight(_posY : float) {
+	posY = _posY;
+}
+
 function Start () {
-	tempBar = GameObject.Find("tempBar");
-	vertices = tempBar.GetComponent(MeshFilter).mesh.vertices;
-	uv = tempBar.GetComponent(MeshFilter).mesh.uv;
+	tempIndicator = GameObject.Find("tempIndicator");
+	vertices = tempIndicator.GetComponent(MeshFilter).mesh.vertices;
+	uv = tempIndicator.GetComponent(MeshFilter).mesh.uv;
 	Cam = GameObject.Find("GUICam");
 	
 	transform.position = new Vector3(transform.position.x * Cam.camera.aspect, transform.position.y, transform.position.z);
     transform.localScale = new Vector3(transform.localScale.x * Cam.camera.aspect, transform.localScale.y, transform.localScale.z);
-
-	temp = GameObject.Find("Environment").GetComponent(Temperature);
 	
+	temp = GameObject.Find("Environment").GetComponent(Temperature);
 	// TOP: 1, 3
 	// BTM: 0, 2
 	
@@ -33,8 +35,15 @@ function Start () {
 }
 
 function Update () {
-	GameObject.Find("tempIndicator").GetComponent(tempIndicator).SetHeight(temp.getTempPercent());
+	vertices[1].y = MAX_BTM + height * posY + 0.2f;
+	vertices[3].y = MAX_BTM + height * posY + 0.2f;
 	
+	vertices[0].y = MAX_BTM + height * posY - 0.2f;
+	vertices[2].y = MAX_BTM + height * posY - 0.2f;
+
+	uv[1].y = 1.0f;
+	uv[3].y = 1.0f;
+		
 	GetComponent(MeshFilter).mesh.vertices = vertices;
 	GetComponent(MeshFilter).mesh.uv = uv;
 }
