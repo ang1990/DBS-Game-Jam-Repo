9e9d7ade9545fc57ddd2c79758d9b37823d1d0ad
sapiangkasa <andika.nma@gmail.com>
2014-12-14 08:57:12 +0800

    Hue changer of mars

---
+++ Assets/HueChanger.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var temperature : Temperature;
+var tempPercent :float;
+var _sprite : SpriteRenderer;
+var initR : float;
+
+function Start () {
+	initR = _sprite.color.r;
+}
+
+function Update () {
+	tempPercent = temperature.getTempPercent();
+	Debug.Log(tempPercent);
+	_sprite.color.r = (initR*tempPercent);
+}
